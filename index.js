567c136d01a5e8ab4fef5ed9270083ebab0b854c
areebaasim01 <161856617+areebaasim01@users.noreply.github.com>
2024-04-23 17:02:41 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+// 31.	No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+// • Remove all of the usernames from your array, and make sure the correct message is printed.
+Object.defineProperty(exports, "__esModule", { value: true });
+let usernames = ["Areeba", "Aiza", "Safia", "Admin", "Fiza", "Reebeow"];
+if (usernames.length === 0) {
+    console.log("We need to find some users!");
+}
+else {
+    for (let i = 0; i < usernames.length; i++)
+        ; // loop condition
+}
+console.log(usernames);
+let usernames1 = [];
+if (usernames1.length === 0) {
+    console.log("We need to find some users!");
+}
+else {
+    for (let i = 0; i < usernames1.length; i++)
+        ;
+}
+console.log(usernames1);
---
+++ index.ts
@@ -0,0 +1,28 @@
+// 31.	No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+// • Remove all of the usernames from your array, and make sure the correct message is printed.
+
+
+let usernames: string[] = ["Areeba", "Aiza", "Safia", "Admin", "Fiza", "Reebeow"];
+
+if(usernames.length ===0){
+    console.log("We need to find some users!");
+} else{
+    for(let i = 0; i< usernames.length; i++);       // loop condition
+}
+
+console.log(usernames)
+
+
+let usernames1: string[] = [];
+
+if(usernames1.length ===0){
+    console.log("We need to find some users!");
+} else{
+    for(let i = 0; i< usernames1.length; i++);
+}
+
+console.log(usernames1)
+
+
+  
